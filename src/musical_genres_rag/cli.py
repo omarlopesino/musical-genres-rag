f50ab65c9139353aa4cc44ea2fe397e1ea14038d
Omar <omarmoper@gmail.com>
2026-07-25 22:10:58 +0200

    One file for all cli commands

--- src/musical_genres_rag/cli.py
+++ src/musical_genres_rag/cli.py
@@ -3,12 +3,15 @@ from musical_genres_rag.Storage import Database
 from musical_genres_rag.Storage import Cache
 from musical_genres_rag.Repository import GenresRepository
 from musical_genres_rag.Index import Index
+from musical_genres_rag.Rag import GenresRag
+
+load_dotenv()
+cache = Cache()
+database = Database()
+grepo = GenresRepository(database, cache)
+table = 'genre_index'
+index = Index(database, table, grepo)
 
 def ingest():
-    load_dotenv()
-    cache = Cache()
-    database = Database()
-    grepo = GenresRepository(database, cache)
-    table = 'genre_index'
-    index = Index(database, table, grepo)
     index.index()
+
