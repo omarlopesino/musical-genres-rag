460cdae7fcbeb693053f998ddb3b0a73a7edaf7e
Omar <omarmoper@gmail.com>
2026-07-26 21:35:09 +0200

    Update cli to use new classes

--- src/musical_genres_rag/cli.py
+++ src/musical_genres_rag/cli.py
@@ -1,16 +1,18 @@
 from dotenv import load_dotenv
-from musical_genres_rag.Storage import Database
+from musical_genres_rag.Storage import PostgresDatabase
 from musical_genres_rag.Storage import Cache
 from musical_genres_rag.Repository import GenresRepository
-from musical_genres_rag.Index import Index
+from musical_genres_rag.Index import Index, PostgresSearchEngine
 from musical_genres_rag.Rag import GenresRag
+import json
 
 load_dotenv()
 cache = Cache()
-database = Database()
+database = PostgresDatabase()
 grepo = GenresRepository(database, cache)
 table = 'genre_index'
-index = Index(database, table, grepo)
+searchEngine = PostgresSearchEngine(database, table)
+index = Index(searchEngine, grepo)
 
 def ingest():
     index.index()
@@ -19,4 +21,4 @@ def rag():
     query = 'What genres could be good If I like electronic music and a lot of drums?';
     genresRag = GenresRag(grepo, index)
     response = genresRag.query(query)
-    print(response.getResponse().output_text)
+    print(response.toJson())
