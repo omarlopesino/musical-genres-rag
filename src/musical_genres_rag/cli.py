82c2f74d800760ffa9247e58b757546ee3d6e8e8
Omar <omarmoper@gmail.com>
2026-07-26 23:39:18 +0200

    Ground truth generation command

--- src/musical_genres_rag/cli.py
+++ src/musical_genres_rag/cli.py
@@ -4,21 +4,26 @@ from musical_genres_rag.Storage import Cache
 from musical_genres_rag.Repository import GenresRepository
 from musical_genres_rag.Index import Index, PostgresSearchEngine
 from musical_genres_rag.Rag import GenresRag
+from musical_genres_rag.Evaluation import GenreQuestionsGroundTruth
 import json
 
 load_dotenv()
 cache = Cache()
 database = PostgresDatabase()
-grepo = GenresRepository(database, cache)
+genreRepository = GenresRepository(database, cache)
 table = 'genre_index'
 searchEngine = PostgresSearchEngine(database, table)
-index = Index(searchEngine, grepo)
+index = Index(searchEngine, genreRepository)
+genresRag = GenresRag(genreRepository, index)
 
 def ingest():
     index.index()
 
 def rag():
-    query = 'What genres could be good If I like electronic music and a lot of drums?';
-    genresRag = GenresRag(grepo, index)
+    query = 'Which genre started alongside rock and roll before becoming more commercially oriented?';
     response = genresRag.query(query)
     print(response.toJson())
+
+def groundTruth():
+    groundTruthGenerator = GenreQuestionsGroundTruth(genreRepository)
+    groundTruthGenerator.generate()
